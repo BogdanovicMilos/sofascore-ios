fb6510fe6d02458f4b5f7ac5c0b612a237d72ebf
BogdanovicMilos <bogdanovic.mlb@gmail.com>
2023-10-15 11:26:15 +0200

    Initial Commit

---
+++ Sofascore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sofascore
+//
+//  Created by Milos Bogdanovic on 15.10.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
